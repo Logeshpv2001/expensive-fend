31edba027005c24f1870f85dab8fad0c4e250d71
Logeshpv2001 <logeshkarthi782@gmail.com>
2025-08-21 17:27:44 +0530

    useEffect added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { message } from "antd";
 import AxiosInstance from "../utilities/AxiosInstance";
 import { useNavigate } from "react-router-dom";
