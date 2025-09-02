2f0120c6aff7a88370566a9d18b95bb57820b29e
Logeshpv2001 <logeshkarthi782@gmail.com>
2025-09-02 11:47:10 +0530

    axios chnaged to railway

--- src/utilities/AxiosInstance.js
+++ src/utilities/AxiosInstance.js
@@ -16,7 +16,9 @@
 import axios from "axios";
 
 const AxiosInstance = axios.create({
-  baseURL: "https://expensive-bend.onrender.com/api", // common base URL
+  // baseURL: "https://expensive-bend.onrender.com/api", // common base URL
+
+  baseURL: "https://expensive-bend-production.up.railway.app",
 
   // baseURL: "http://localhost:5000/api",
   headers: {
